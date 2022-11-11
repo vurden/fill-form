bc3b30007bb7ab568e1d10fe00874b1578ed0d7a
a123 <a123@a123s-Air.home>
2022-11-11 12:20:07 -0500

    working messages

--- background.js
+++ background.js
@@ -1,13 +1,37 @@
-// Listen for user to press activate extension and execute 
-chrome.storage.sync.set({forms: {}})
-chrome.action.onClicked.addListener(function(tab) {
-  try {chrome.storage.sync.get(["forms"], function(result) {
-    console.log(result)
-  })}
-  catch {chrome.storage.sync.set({forms: {}}) }
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting === "hello")
+      sendResponse({farewell: "goodbye"});
+  }
+);
 
-  chrome.scripting.executeScript({
-      target: {tabId: (tab.id)},
-      files: ['script.js']
-  });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Listen for user to press activate extension and execute 
+// chrome.action.onClicked.addListener(function(tab) {
+//   try {chrome.storage.sync.get(["forms"], function(result) {})}
+//   catch {chrome.storage.sync.set({forms: {}}) }
+
+  
+  // Remove "default_popup": "index.html" attribute to action key in manifest to use this code.
+  // chrome.scripting.executeScript({
+  //     target: {tabId: (tab.id)},
+  //     files: ['script.js']
+  // });
+// });
--- script.js
+++ script.js
@@ -1,48 +1,69 @@
-function main() {
+// function main() {
 
-  page_url = document.URL
-  chrome.storage.sync.get(["forms"], function(result) {
-    console.log(result.page_url)
-    if (result.page_url == undefined) {
-      console.log("Form not found. Creating form...")
-      form = scrapeForm();
-      console.log("Form created. Archiving form...")
-      archive(form, result.forms)
-      console.log("Form archived.")
-    } else {
-      console.log("Form found. Filling form...")
-      for (key in result.page_url) {
-        document.getElementById(key).value = result.page_url.key
-      }
-    }
-  })
-};
+//   page_url = document.URL
+//   chrome.storage.sync.get(["forms"], function(result) {
+//     console.log(result.page_url)
+//     if (result.page_url == undefined) {
+//       console.log("Form not found. Creating form...")
+//       form = scrapeForm();
+//       console.log("Form created. Archiving form...")
+//       archive(form, result.forms)
+//       console.log("Form archived.")
+//     } else {
+//       console.log("Form found. Filling form...")
+//       for (key in result.page_url) {
+//         document.getElementById(key).value = result.page_url.key
+//       }
+//     }
+//   })
+// };
 
-function archive(form, state) {
-  console.log("state")
-  console.log(state)
-  url = Object.keys(form)[0]
-  state.url = form.url
-  chrome.storage.sync.set({forms: state})
-};
+// function archive(form, state) {
+//   console.log("state")
+//   console.log(state)
+//   url = Object.keys(form)[0]
+//   state.url = form.url
+//   chrome.storage.sync.set({forms: state})
+// };
 
 
 
-function scrapeForm() {
-  input_fields = document.getElementsByTagName("input");
+// function scrapeForm() {
+//   input_fields = document.getElementsByTagName("input");
   
-  id_value_object = {};
-  for (const field of input_fields) {
-    if (field.type == "hidden") continue;
-    if (field.id == "") continue;
-    id_value_object[field.id] = field.value;
-  };
+//   id_value_object = {};
+//   for (const field of input_fields) {
+//     if (field.type == "hidden") continue;
+//     if (field.id == "") continue;
+//     id_value_object[field.id] = field.value;
+//   };
 
-  url = document.URL
-  console.log(url)
-  page_object = {}[url] = id_value_object
+//   url = document.URL
+//   console.log(url)
+//   page_object = {}[url] = id_value_object
 
-  return page_object;
-};
+//   return page_object;
+// };
 
-main(); 
+// main(); 
+
+function save() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+      console.log(response.farewell);
+    });
+  });
+}
+
+function load() {
+  console.log("load");
+  alert("load")
+}
+
+
+function main() {
+  document.getElementById("save").addEventListener("click", save);
+  document.getElementById("load").addEventListener("click", load);
+}
+
+main()
